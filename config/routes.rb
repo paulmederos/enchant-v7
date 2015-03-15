62fbb78af7c5c93474048cb8555e3531dde5adbd
Paul Mederos <pvm@enchant.co>
2015-03-15 14:47:47 -0700

    RSS fix

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'pages#home'
 
+  get '/brittany', to: 'pages#brittany'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
